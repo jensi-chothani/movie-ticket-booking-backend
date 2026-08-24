39435360767196dc4d3a64eab2c10cbc06870440
Jensi Chothani <jensichothani43@gmail.com>
2026-08-24 14:58:15 +0530

    Modify CORS to allow Vercel subdomains
    Updated CORS settings to allow subdomains on Vercel.

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://movie-ticket-booking-frontend-adgndgxqk-movie21.vercel.app"
     ],
+    allow_origin_regex=r"https://movie-ticket-booking-frontend.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
